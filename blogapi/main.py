29c7f4163b544f1dfd4c2ff128f968c51bf2b255
Shah rahman <toshah.rahman@gmail.com>
2022-05-29 12:57:39 +0300

    fastapi fun 10

--- blogapi/main.py
+++ blogapi/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from schemas import User
+
 from .routes import users
 
 app = FastAPI(title="BlogAPI")
 
 
 app.include_router(users.router)
+
+
--- blogapi/routes/users.py
+++ blogapi/routes/users.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
+from ..schemas import User, db
 
 router = APIRouter(tags=["Users"])
 
@@ -6,3 +8,8 @@ router = APIRouter(tags=["Users"])
 @router.get("/index")
 def index():
     pass
+
+
+@router.post("/registration")
+async def registration(user_info: User):
+    user_info = jsonable_encoder(user_info)
