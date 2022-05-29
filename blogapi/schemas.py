855588326d9dea726375c052c7b90960f5b328ad
Shah rahman <toshah.rahman@gmail.com>
2022-05-29 12:28:51 +0300

    fastapi fun 7

--- blogapi/schemas.py
+++ blogapi/schemas.py
@@ -9,13 +9,18 @@ client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGODB_URL"))
 
 db = client.blogapi
 
+
 class PyObjectid(ObjectId):
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
 
     @classmethod
-    def validate(cls,v):
+    def validate(cls, v):
         if not ObjectId.is_valid(v):
             raise ValueError('Invalid ObjectID')
         return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
