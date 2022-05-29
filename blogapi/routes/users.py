06726a0dd14afb68b22e4ae6b2c45e5a8468b930
Shah rahman <toshah.rahman@gmail.com>
2022-05-29 13:57:36 +0300

    fastapi fun 11

--- blogapi/routes/users.py
+++ blogapi/routes/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException,status
 from fastapi.encoders import jsonable_encoder
 from ..schemas import User, db
 
@@ -13,3 +13,14 @@ def index():
 @router.post("/registration")
 async def registration(user_info: User):
     user_info = jsonable_encoder(user_info)
+
+    username_found = await db['users'].find_one({'name':user_info['name']})
+    email_found = await db['users'].find_one({'email':user_info['email']})
+
+    if username_found:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail='username already taken')
+
+    if email_found:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail='email already taken')
