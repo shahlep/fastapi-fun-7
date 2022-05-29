9155db3eba66151609e20b1ff30de324cb5c2fff
Shah rahman <toshah.rahman@gmail.com>
2022-05-29 12:24:15 +0300

    fastapi fun 6

--- blogapi/schemas.py
+++ blogapi/schemas.py
@@ -1,9 +1,21 @@
 from dotenv import load_dotenv
 import motor.motor_asyncio
 import os
+from bson import ObjectId
 
 load_dotenv()
 
 client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGODB_URL"))
 
 db = client.blogapi
+
+class PyObjectid(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls,v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid ObjectID')
+        return ObjectId(v)
