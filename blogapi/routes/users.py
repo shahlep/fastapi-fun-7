f4a9cf82829656d6ece848209937353b61c47a71
Shah rahman <toshah.rahman@gmail.com>
2022-05-29 18:08:16 +0300

    fastapi fun 17

--- blogapi/routes/users.py
+++ blogapi/routes/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 from fastapi.encoders import jsonable_encoder
-from ..schemas import User, db
+from ..schemas import User, db, UserResponse
 from ..utils import get_password_hash
 import secrets
 
@@ -12,7 +12,7 @@ def index():
     pass
 
 
-@router.post("/registration")
+@router.post("/registration",response_model=UserResponse)
 async def registration(user_info: User):
     user_info = jsonable_encoder(user_info)
 
